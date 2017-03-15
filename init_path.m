cd49485b3b209d8c73d595771c978c500d6e1b63
Will Price <will.price94@gmail.com>
2017-03-15 21:13:44 +0000

    Refactor hard coded paths to dynamic in init_path.m

--- init_path.m
+++ init_path.m
@@ -1,9 +1,11 @@
-disp('adding necessary paths')
+disp('Adding necessary paths')
 % adapt this to your local paths
 % this allows Matlab to find the used functions
-addpath('/Users/hauser/Matlab/Morphological_Computation_Tutorial');
-addpath('/Users/hauser/Matlab/Morphological_Computation_Tutorial/helping_functions');
-addpath('/Users/hauser/Matlab/Morphological_Computation_Tutorial/data_vanderPol');
-addpath('/Users/hauser/Matlab/Morphological_Computation_Tutorial/data_Volterra');
-addpath('/Users/hauser/Matlab/Morphological_Computation_Tutorial/data_quad');
-addpath('/Users/hauser/Matlab/Morphological_Computation_Tutorial/data_NARMA');
+current_script_path = mfilename('fullpath');
+[script_dir,~,~] = fileparts(current_script_path);
+addpath(script_dir);
+addpath(fullfile(script_dir, 'helping_functions'));
+addpath(fullfile(script_dir, 'data_vanderPol'));
+addpath(fullfile(script_dir, 'data_Volterra'));
+addpath(fullfile(script_dir, 'data_quad'));
+addpath(fullfile(script_dir, 'data_NARMA'));
